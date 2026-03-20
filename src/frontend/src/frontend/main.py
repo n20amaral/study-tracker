96aecc7ed30dba556486d6845a8319b3142e561d
Nuno Amaral <n20amaral@outlook.com>
2026-03-20 19:10:29 -0100

    feat(frontend): added frontend docker build and push action to the workflow (#11)

--- src/frontend/src/frontend/main.py
+++ src/frontend/src/frontend/main.py
@@ -126,5 +126,7 @@ def main():
     app.run(host=app.config["HOST"], port=app.config["PORT"], debug=app.config["DEBUG"])
 
 
+# Github Actions Trigger Marker
+
 if __name__ == "__main__":
     main()
